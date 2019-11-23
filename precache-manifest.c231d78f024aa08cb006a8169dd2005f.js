82133697007b576d2aeaba97003f748a72040f79
Hellen Ward <hellenwellsward@gmail.com>
2019-11-23 19:28:47 +0000

    Updates

---
+++ precache-manifest.c231d78f024aa08cb006a8169dd2005f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac4513477a8cd2f72772825c9bc2ded",
+    "url": ":https://hellenward.github.io/colorchange/index.html"
+  },
+  {
+    "revision": "bfcb605e6f46e890c429",
+    "url": ":https://hellenward.github.io/colorchange/static/css/main.2c17629b.chunk.css"
+  },
+  {
+    "revision": "002da8cd5a06d12bd2a6",
+    "url": ":https://hellenward.github.io/colorchange/static/js/2.f570a5d9.chunk.js"
+  },
+  {
+    "revision": "bfcb605e6f46e890c429",
+    "url": ":https://hellenward.github.io/colorchange/static/js/main.67fd99b5.chunk.js"
+  },
+  {
+    "revision": "abd698d06df70dc19c72",
+    "url": ":https://hellenward.github.io/colorchange/static/js/runtime-main.8f90d1d2.js"
+  }
+]);
