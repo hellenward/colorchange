ac747587928cac67d3eca804ec6cbe813f4b3ab9
Howard Yeend <h.yeend@growinginteractive.com>
2019-12-22 10:19:25 +0000

    We can remove the constructor now too

--- src/components/masterbutton.jsx
+++ src/components/masterbutton.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 class MasterButton extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   handleClick = color => {
     document.body.style.backgroundColor = color;
